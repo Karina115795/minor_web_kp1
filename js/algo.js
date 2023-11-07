fea0dd135d5f3b0cd902b363d27179fd7d1ecb4a
Karina Iksanova <k.r.iksanova@gmail.com>
2023-11-07 23:44:59 +0300

    work done

---
+++ js/algo.js
@@ -0,0 +1,19 @@
+function calc_place() {
+    var inp = document.getElementById('place')
+    var place = inp.valueAsNumber
+
+    var result = 'Некорректное значение'
+    if (place > 36 & place <= 54) {
+        if (place % 2 == 0) result = 'Боковое верхнее место'
+        else result = 'Боковое нижнее место'
+    } else if (place > 0 & place <= 36){
+        if (place % 2 == 0) result = 'Купе верхнее место'
+        else result = 'Купе нижнее место'
+    }
+
+    var span = document.getElementById('result')
+    span.innerText = result
+}
+
+var button = document.getElementById('calc')
+button.addEventListener('click', calc_place)
